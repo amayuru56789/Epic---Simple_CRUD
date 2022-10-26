03f2c66af40ff06fa7494255227022f5ec62470e
amayuru56789 <indeewara5678@gmail.com>
2022-10-27 01:03:40 +0530

    added DbConnection for SimpleCrud

---
+++ SimpleCrud/src/main/java/com/mycompany/simplecrud/db/DbConnection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.simplecrud.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author indee
+ */
+public class DbConnection {
+    
+    public Connection connection;
+
+    public DbConnection() {
+    }
+    
+    public Connection getConnection() throws ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/databaseName", "root", "1234");
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+    
+    public void connectionClose() throws SQLException{
+        connection.close();
+    }
+}
